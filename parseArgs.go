820000cae5f75f32c3b283bbad0f9e38ca2c76fe
CorentinB <corentin.barreau@epitech.eu>
2018-12-05 19:35:17 +0100

    Modify: parseArgs - Fix typo.

--- parseArgs.go
+++ parseArgs.go
@@ -13,30 +13,36 @@ func parseArgs(args []string) {
 		os.Exit(1)
 	} else if len(args) == 6 {
 		// File path
-		config.FilePath = args[1]
+		config.FilePath = args[0]
 
 		// Redis host
-		config.Host = args[2]
+		config.Host = args[1]
 
 		// Redis Password
-		config.Password = args[3]
+		config.Password = args[2]
 
 		// Redis DB
-		if _, err := strconv.ParseInt(args[4], 10, 64); err == nil {
-			config.DB, _ = strconv.ParseInt(args[4], 10, 64)
+		if _, err := strconv.ParseInt(args[3], 10, 64); err == nil {
+			config.DB, _ = strconv.ParseInt(args[3], 10, 64)
 		} else {
-			color.Red("Usage: ./youtube-ig [FILE] [REDIS-HOST] [REDIS-PASSWORD] [REDIS-DB] [REDIS-KEY] [CONCURRENCY]")
+			color.Red("Bad argument for DB: " + args[3])
+			color.Red("Usage: ./TXToRedis [FILE] [REDIS-HOST] [REDIS-PASSWORD] [REDIS-DB] [REDIS-KEY] [CONCURRENCY]")
 			os.Exit(1)
 		}
 
 		// Redis Key
-		config.Key = args[5]
+		config.Key = args[4]
 
-		if _, err := strconv.ParseInt(args[6], 10, 64); err == nil {
-			config.Concurrency, _ = strconv.ParseInt(args[6], 10, 64)
+		if _, err := strconv.ParseInt(args[5], 10, 64); err == nil {
+			config.Concurrency, _ = strconv.ParseInt(args[5], 10, 64)
 		} else {
-			color.Red("Usage: ./youtube-ig [FILE] [REDIS-HOST] [REDIS-PASSWORD] [REDIS-DB] [REDIS-KEY] [CONCURRENCY]")
+			color.Red("Bad argument for concurrency: " + args[5])
+			color.Red("Usage: ./TXToRedis [FILE] [REDIS-HOST] [REDIS-PASSWORD] [REDIS-DB] [REDIS-KEY] [CONCURRENCY]")
 			os.Exit(1)
 		}
+	} else {
+		color.Red("Only " + string(len(args)) + " specified.")
+		color.Red("Usage: ./TXToRedis [FILE] [REDIS-HOST] [REDIS-PASSWORD] [REDIS-DB] [REDIS-KEY] [CONCURRENCY]")
+		os.Exit(1)
 	}
 }
